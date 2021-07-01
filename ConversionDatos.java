f9689c1d7b1058da2237675ac8c3fc48c4254a8c
joelv9000 <joelvelazquezvargasmm@gmail.com>
2021-07-01 11:27:29 -0500

    primer commit proyecto Java

---
+++ ConversionDatos.java
@@ -0,0 +1,16 @@
+
+public class ConversionDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/* tipos de datos em Java
+		 * Numericos:byte, short ,int, long, float, double
+		 * Booleanos (verdaddero, falso):Bollean
+		 * Characteres :char
+		 * Cadenas : String
+		 * 
+		 */
+	}
+
+}
